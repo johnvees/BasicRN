f0aa0396c009c75033d00afc1a2ed40692c750ee
Yohanes <yohanesvelly23@gmail.com>
2021-11-29 12:07:25 +0700

    update eslint rules

---
+++ test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const test = () => {
+  return (
+    <View>
+      <Text>sdfsfsdf</Text>
+    </View>
+  );
+};
+
+export default test;
+
+const styles = StyleSheet.create({});
